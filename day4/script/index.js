036955e37458930704adc981fe441eed26460454
Lokesh777 <lokeshdevgan777@gmail.com>
2022-07-23 07:44:35 +0530

    Integration page completed with next page

---
+++ day4/script/index.js
@@ -0,0 +1,450 @@
+
+var data= [
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/airtable.png",
+      title: "Airtable",
+      description: "Powerful organizational spreadsheets that works exactly the way you want.",
+      id: "7AHhCpv"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/anydo.png",
+      title: "Any.Do",
+      description: "Any.Do helps you get things done with your friends in a simple & elegant way.",
+      id: "1hC9KDp"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/asana.png",
+      title: "Asana",
+      description: "Asana puts tasks and conversations together to enable teamwork without email.",
+      id: "hdUMaKZ"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/azendoo.png",
+      title: "Azendoo",
+      description: "Simplifies projects planning, documents sharing and teamwork synchronization for both your professional activity and your life.",
+      id: "6VMSTq_"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/azure-devops.png",
+      title: "Azure DevOps",
+      description: "Continuous Delivery Services for teams to share code, track work, and ship software.",
+      id: "MiNrIcl"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/basecamp-2.png",
+      title: "Basecamp 2",
+      description: "Basecamp is a web-based project collaboration tool that allows groups of people to share files.",
+      id: "-NJ8OKx"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/basecamp-2.png",
+      title: "Basecamp 3",
+      description: "Basecamp 3 is a web-based project collaboration tool that allows groups of people to share files.",
+      id: "9vBov-I"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/bitbucket.png",
+      title: "Bitbucket",
+      description: "Bitbucket is a code hosting site, for the Git and Mercurial version control systems.",
+      id: "F--Lb7Z"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/azure-devops.png",
+      title: "clickup",
+      description: "Docs, Reminders, Goals, Calendars, Chat, scheduling and more.",
+      id: "4FSjWkF"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/coda-time-tracking.png",
+      title: "Coda",
+      description: "Coda blends the flexibility of a document, the power of a spreadsheet.",
+      id: "jzKhuGA"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/dixa-time-tracking.png",
+      title: "Dixa",
+      description: "Customer service platform that unifies phone, email, chat and messaging apps.",
+      id: "UtDQ2Yj"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/evernote-time-tracking.png",
+      title: "Evernote",
+      description: "Create documents, collaborate on projects, and store information all in one place.",
+      id: "ch6G-1k"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/favro.png",
+      title: "Favro",
+      description: "Balance power and simplicity in the ideal collaboration app for developers.",
+      id: "wppsRJQ"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/figma-time-tracking.png",
+      title: "Figma",
+      description: "Cloud-based platform that enables businesses to create custom designs.",
+      id: "NcOIcXg"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/flow.svg",
+      title: "Flow",
+      description: "Manage tasks, share files, have discussions, and capture ideas all in one place.",
+      id: "F9iloYu"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/freshdesk.png",
+      title: "Freshdesk",
+      description: "Freshdesk is an award-winning, online help desk software that allows you to support customers.",
+      id: "EJ9mH_f"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/github.png",
+      title: "GitHub",
+      description: "GitHub is a web-based hosting service for projects that use the Git revision control system.",
+      id: "P-bjxm-"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/gitlab.png",
+      title: "GitLab",
+      description: "GitLab is a on-premise or hosted Git repository management tool.",
+      id: "vZ6MwYM"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/ring-central-time-tracking.png",
+      title: "Ring Central",
+      description: "Conversation platform for teams to plan, share & organize work tasks.",
+      id: "U5VZAQQ"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/gmail.png",
+      title: "Gmail",
+      description: "Gmail is a free search-based web-mail service by Google.",
+      id: "FqvUuYI"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/google-calendar.png",
+      title: "Google Calendar",
+      description: "With Google Calendar, its easy to keep track of all your lifes important events in one place.",
+      id: "gDOKVPm"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/gdocs.png",
+      title: "Google Docs",
+      description: "Google Docs is an online word processor, part of Google Drive.",
+      id: "NZaD_nv"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/gsheets.png",
+      title: "Google Sheets",
+      description: "Google Sheets is an online spreadsheet app that lets you create and format spreadsheets.",
+      id: "5xPfK3W"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/google-drive.png",
+      title: "Google Slides",
+      description: "Google Slides is a presentation app offered by Google.",
+      id: "UXRBNVh"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/gtasks.png",
+      title: "Google Tasks",
+      description: "Tasks adds a to-do list to Gmail",
+      id: "7fICRXg"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/google-chat.png",
+      title: "Google Chat",
+      description: "Collaborate on Google chat",
+      id: "s68p0EO"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/google-workspace.png",
+      title: "Google Workspace",
+      description: "Google Workspace consists of Gmail, Contacts, Calendar, Meet and Chat for communication.",
+      id: "I2RF4o-"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/insightly.png",
+      title: "Insightly",
+      description: "Web-based project and Customer Relationship Management (CRM) application.",
+      id: "6hsMdha"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/jira.png",
+      title: "Jira",
+      description: "JIRA is the leading bug tracking, issue tracking and project management tool.",
+      id: "Xg_ka8V"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/linear-time-tracking.png",
+      title: "Linear",
+      description: "The issue tracking tool you'll enjoy using.",
+      id: "NTRjZ88"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/meistertask-time-tracking.png",
+      title: "Meistertask",
+      description: "Collaboration and task management tool on the web.",
+      id: "KDot7pL"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/microsoft-to-do.png",
+      title: "Microsoft To Do",
+      description: "Microsoft To Do is a task management app to help you stay organized and manage your day-to-day.",
+      id: "k3egQQY"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/microsoft-planner.png",
+      title: "Microsoft Planner",
+      description: "Microsoft Planner lets you easily bring together teams, tasks, documents, and conversations for better results.",
+      id: "AfqSRLH"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/monday.png",
+      title: "Monday",
+      description: "Monday is a project management tool your team will actually enjoy using.",
+      id: "pDgxXiE"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/nifty-time-tracking.png",
+      title: "Nifty",
+      description: "Task management app to manage tasks, timelines and docs.",
+      id: "skdIlgp"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/notion.png",
+      title: "Notion",
+      description: "Notion is the all-in-one workspace for notes, project management, documents, and collaboration.",
+      id: "AqcGzhm"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/outlook.png",
+      title: "Outlook.com",
+      description: "Outlook.com is a webmail service from Microsoft, successor of Hotmail.",
+      id: "M4ACGXj"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/podio.png",
+      title: "Podio",
+      description: "Podio lets you build and shape the online workplace most fitting to your role.",
+      id: "1GlLKx_"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/redbooth.png",
+      title: "Redbooth",
+      description: "Redbooth (formerly Teambox), the cloud-based company redefining collaboration and busines communications.",
+      id: "7wSQ1lG"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/redmine.png",
+      title: "Redmine",
+      description: "Online project management software built on the Ruby on Rails framework specifically for small groups.",
+      id: "zU9_t3R"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/remember-the-milk.png",
+      title: "Remember the milk",
+      description: "Take your tasks anywhere with this feature-packed application.",
+      id: "Wt0N3Mi"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/smartsheet.png",
+      title: "Smartsheet",
+      description: "Smartsheet is a web-based project and task management and work collaboration software.",
+      id: "QowxKB7"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/slack.png",
+      title: "Slack",
+      description: "Slack brings all your communication together in one place.",
+      id: "XxkZ5Pb"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/SupportPal.png",
+      title: "Supportpal",
+      description: "Powerful self-hosted help desk solution that is simple and intuitive to use.",
+      id: "d1A4BaJ"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/taskade.png",
+      title: "Taskade",
+      description: "Taskade is reimagining the to-do list by making it visual, structured, and collaborative.",
+      id: "Dacgtki"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/teams.png",
+      title: "Microsoft Teams",
+      description: "Microsoft Teams is the chat-based workspace that integrates all the people, content, and tools your team needs.",
+      id: "ittpjcu"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/teamwork-time-tracking.png",
+      title: "Teamworks",
+      description: "Teamwork is project management software.",
+      id: "rEObB4M"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/ticktick.png",
+      title: "Tick Tick",
+      description: "Your daily must-have to-do & task list to make all things done and get life well organized.",
+      id: "jCOhVed"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/todoist.png",
+      title: "Todoist",
+      description: "Todoist is a task manager thats useful, fast and easy to use.",
+      id: "MEgIg5X"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/trello.png",
+      title: "Trello",
+      description: "Trello is a collaboration tool that organizes your projects into boards.",
+      id: "hODwTjK"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/temp/logos/zendesk.png",
+      title: "Zendesk",
+      description: "Offers on-demand help desk software, covering customer support, trouble tickets and knowledge base.",
+      id: "v1V6OZZ"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/activecollab-time-tracking.png",
+      title: "Activecollab",
+      description: "Project Management Tool in the Office & Productivity category.",
+      id: "EpJ9JfE"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/agiled-time-tracking.png",
+      title: "Agiled",
+      description: "Project Management software",
+      id: "2tsSpU9"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/assembla-time-tracking.png",
+      title: "Assembla",
+      description: "Online workspaces with best of breed tools like Tasks & Issue Management, SVN & Git Repositories.",
+      id: "LhF-GyR"
+    },
+    {
+      image: "https://trackingtime.co/wp-content/themes/trackingtime-v4/img/button/axosoft-time-tracking.png",
+      title: "Axosoft",
+      description: "Agile based projecment management software.",
+      id: "8QyShxi"
+    }
+  ]
+let id;
+let count=0;
+
+  localStorage.setItem("storedData",JSON.stringify(data));
+
+  var cartData=JSON.parse(localStorage.getItem("storedData"))||[];  
+// console.log('cartData:', cartData)
+  let selected_Card=[];
+
+
+
+ 
+// appending create elemnet here
+function Cid(xxx){
+    return document.createElement(xxx);
+}
+function append(data){
+  // console.log('data:', data)
+  let cont=document.getElementById('container');
+   cont.innerHTML=null
+//    cont.innerHTML=null
+ 
+  data.forEach(ele => {
+   
+    let div=Cid("div");
+    div.id= "appCard"
+    div.addEventListener("click",function(){
+      nextPage(ele);
+  })
+    
+    let div2=Cid("figure");
+    div2.id="appLogo"
+   
+    let img=Cid("img");
+    img.src=ele.image;
+
+    div2.append(img)
+
+    let title=Cid("h3")
+    title.innerText=ele.title;
+
+    let description = Cid("p");
+    description.innerText=ele.description;
+
+    div.append(div2,title,description)
+    cont.append(div);
+  });
+
+
+}
+  
+   
+
+  
+// <------------next page after click---------->
+let nextPage=(ele)=>{
+  selected_Card.push(ele)
+  localStorage.setItem("salected_Card",JSON.stringify(selected_Card));
+  window.location.href="./nextPage.html"
+}
+// <----------------debouncing---------------->
+
+  
+
+document.getElementById('search').addEventListener("keydown", search)
+
+function search(){
+
+  let cont=document.getElementById('container');
+  cont.innerHTML=null
+         let search= document.getElementById('search').value;
+console.log('search:', search)
+        
+     for(let i=0;i<cartData.length;i++){
+      
+            if(search==cartData[i].title) {
+              let ele= cartData[i]
+              alert("thank you for trusting on us !!! ")
+
+                // cont.append(JSON.stringify(cartData[i]))
+                // console.log(cartData[i])
+                // count++;
+                // break;
+              
+               
+                  let div=Cid("div");
+                  div.id= "appCard"
+                  div.addEventListener("click",function(){
+                    nextPage(ele);
+                })
+                  
+                  let div2=Cid("figure");
+                  div2.id="appLogo"
+                 
+                  let img=Cid("img");
+                  img.src=ele.image;
+              
+                  div2.append(img)
+              
+                  let title=Cid("h3")
+                  title.innerText=ele.title;
+              
+                  let description = Cid("p");
+                  description.innerText=ele.description;
+              
+                  div.append(div2,title,description)
+                  cont.append(div);
+                
+              
+              }
+          }
+} 
+      
+// if(count==0){
+  
+append(cartData)
+// }
